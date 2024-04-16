effd3664bc4d2c15e977bada343a42cb574f30f0
Sam <muigai.samuel21@students.dkut.ac.ke>
2024-04-16 21:41:41 +0300

    Initial commit

---
+++ app/src/main/java/com/samkt/pawtrait/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.samkt.pawtrait
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.samkt.pawtrait.ui.theme.PawtraitTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PawtraitTheme {
+            }
+        }
+    }
+}
+
